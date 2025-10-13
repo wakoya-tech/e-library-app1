47ce77f98f8f0c71fbb2308e9fdd10c7a977bb45
wakoya-tech <waaqoyyaadhaabaa@gmail.com>
2025-10-13 11:47:29 +0300

    add

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,13 +1,23 @@
-import BookForm from "../form/BookForm.jsx"
-const AddBook = () =>{
-    const handleOnSubmit=(book)=>{
-        console.log(book);
-    }
-    return(
-        <div className="addForm">
-            <BookForm handleSubmit={handleOnSubmit}/>
-        </div>
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { BookContext } from "../context/BookContext.jsx";
+import BookForm from "../form/BookForm.jsx";
 
-    )
+const AddBook = () => {
+  const navigate = useNavigate();
+  
+  // useContext to get books and setBooks from your context
+  const { books, setBooks } = useContext(BookContext);
+
+  const handleOnSubmit = (book) => {
+    setBooks([book, ...books]);
+    navigate('/');
+  }
+
+  return (
+    <div className="addForm">
+      <BookForm handleSubmit={handleOnSubmit} />
+    </div>
+  );
 }
-export default AddBook
+export default AddBook;
