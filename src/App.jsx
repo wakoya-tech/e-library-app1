4645ced34bf290fb4202c03a5c011df90da922e0
wakoya-tech <waaqoyyaadhaabaa@gmail.com>
2025-10-13 16:25:22 +0300

    update all changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import ListBook from './components/ListBook.jsx';
 import AddBook from './components/AddBook.jsx';
 import BookProvider from './context/BookContext.jsx';
+import EditBook from './components/EditBook.jsx';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path='/' element={<ListBook />} />
           <Route path='/add' element={<AddBook />} />
+          <Route path='/edit/:id' element={<EditBook />} />
         </Routes>
       </div>
     </BookProvider>
