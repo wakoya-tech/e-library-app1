60249302fbbf9360841b9609c4807ff4e6321519
wakoya-tech <waaqoyyaadhaabaa@gmail.com>
2025-10-13 16:20:53 +0300

    add Edit Book

---
+++ src/components/EditBook.jsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { BookContext } from "../context/BookContext.jsx";
+import BookForm from "../form/BookForm.jsx";
+
+const EditBook = () => {
+    const {id}=useParams();
+  const navigate = useNavigate();
+  
+  // useContext to get books and setBooks from your context
+  const { books, setBooks } = useContext(BookContext);
+
+  const handleOnSubmit = (book) => {
+    setBooks([book, ...books]);
+    navigate('/');
+  }
+
+  return (
+    <div className="addForm">
+      <BookForm handleSubmit={handleOnSubmit} />
+    </div>
+  );
+}
+export default EditBook;
