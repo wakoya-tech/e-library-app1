804115d19a74a2b3ef4b0a72a7673c93d41d06b1
Wakoya Daba <waaqoyyaadhaabaa@gmail.com>
2025-11-19 09:12:49 +0300

    Update BookContext.jsx

--- src/context/BookContext.jsx
+++ src/context/BookContext.jsx
@@ -2,7 +2,6 @@ import { createContext, useState } from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
 
 export const BookContext = createContext();
-
 const BookProvider = ({ children }) => {
   const [books, setBooks] = useLocalStorage('books',[]);
   const value = { books, setBooks };
