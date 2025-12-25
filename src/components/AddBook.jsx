a53d5d65e38bdb5259b75e1b379118fdca37706c
Wakoya Daba <waaqoyyaadhaabaa@gmail.com>
2025-12-25 21:20:27 +0300

    Update AddBook.jsx

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -6,6 +6,7 @@ const AddBook = () => {
   const navigate = useNavigate();
   // useContext to get books and setBooks from your context
   const { books, setBooks } = useContext(BookContext);
+  
   const handleOnSubmit = (book) => {
     setBooks([book, ...books]);
     navigate('/');
